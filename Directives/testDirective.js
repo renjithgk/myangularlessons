21386a6ea140c7636d45afe18075b031e319295e
Renjith <renjithkumar1@gmail.com>
2016-10-18 23:56:37 +0800

    Directive, Factory added

---
+++ Directives/testDirective.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="../angular.js" />
+
+
+var app = angular.module('app');
+
+app.directive('testDirective', function () {
+    return {
+        template: '<h1>Hello Directive</h1>'
+    };
+});
+
+//Scope
+//https://www.undefinednull.com/2014/02/11/mastering-the-scope-of-a-directive-in-angularjs/
--- Feature1/Service1.js
+++ Feature1/Service1.js
@@ -22,3 +22,32 @@ app.service('Service1', function ($http, $q) {
     }
 
 });
+
+
+//factory and service both are singleton
+
+https://weblogs.asp.net/dwahlin/using-an-angularjs-factory-to-interact-with-a-restful-service
+
+
+app.service('myService', function () {
+
+        // service is just a constructor function
+        // that will be called with 'new'
+
+        this.sayHello = function (name) {
+            return "Hi " + name + "!";
+        };
+    });
+
+app.factory('myFactory', function () {
+
+    // factory returns an object
+    // you can run some code before
+
+    return {
+        sayHello: function (name) {
+            return "Hi " + name + "!";
+        }
+    }
+});
+
